cb2716852b637b4865d8175061d5b3b12f28db43
Thomas Bandy <thomas.bandy@outlook.com>
2025-06-08 19:43:33 -0400

    Added promiseVersion

---
+++ promiseVersion.js
@@ -0,0 +1,25 @@
+// person's name
+const person = "Person"
+
+// call function
+mowYard()
+
+// mowing
+const mowYard = Promise((resolve) => {
+    setTimeout(function() {
+        resolve(person + " mowed the yard.")
+    }, 2000);
+});
+// weedeating
+
+
+//  hedge trimming
+
+
+// collect wood
+
+
+// water garden
+
+
+//
